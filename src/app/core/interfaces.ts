fa73af330069a7d43f4f788964c6c086408a9ee5
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-06-16 16:20:26 +0300

    added air quality IForecast

--- src/app/core/interfaces.ts
+++ src/app/core/interfaces.ts
@@ -44,5 +44,15 @@ export interface IForecast {
         uv: number
         gust_mph: number,
         gust_kph: number
+        air_quality: {
+            co: number,
+            no2: number,
+            o3: number,
+            so2: number,
+            pm2_5: number,
+            pm10: number,
+            'us-epa-index': number,
+            "gb-defra-index": number
+        }
     }
 }
