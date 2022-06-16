af73c3f9dff564d902f39ecadd80c0c23bd1db70
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-06-16 16:07:58 +0300

    updated service

--- src/app/core/service/forecast.service.ts
+++ src/app/core/service/forecast.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { IForecast } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +10,26 @@ export class ForecastService {
 
   url: string = environment.baseUrl;
   key: string = environment.secretKey;
-  autoLocation: string = 'auto:ip';
-
+  
   
   constructor(  
     private http: HttpClient,
   ) { }
 
   getCurrentLocationForecast(location: any) {
-    return this.http.get<any>(`${this.url}/current.json?key=${this.key}&q=${location ? location : this.autoLocation}`);
+    return this.http.get<any>(`${this.url}/current.json?key=${this.key}&q=${location}&aqi=yes&lang=bg`);
+  }
+
+  airQualityCode() {
+    return {
+      1: 'Добро', 
+      2: 'Умерено', 
+      3: 'Нездравословно за определени групи',
+      4: 'Нездравословно',
+      5: 'Много Нездравословно', 
+      6: 'Опасно'
+    }
   }
 
 }
+
