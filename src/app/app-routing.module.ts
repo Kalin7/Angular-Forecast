0771b4720215a3a4f336474bb615535755c2b5bc
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-06-16 16:25:19 +0300

    added new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'forecast',
+    loadChildren: () => import('./feature/feature-routing.module').then((m) => m.FeatureRoutingModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
