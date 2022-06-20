19297ed966ac895f1fb69e07fe9ba54bc0341cd2
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-06-20 14:07:44 +0300

    added hook ngOnDestroy

--- src/app/feature/daily/daily.component.ts
+++ src/app/feature/daily/daily.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { IForecast } from 'src/app/core/interfaces';
 import { ForecastService } from 'src/app/core/service/forecast.service';
-import { HeaderService } from 'src/app/core/service/header.service';
 
 
 @Component({
@@ -9,28 +10,39 @@ import { HeaderService } from 'src/app/core/service/header.service';
   templateUrl: './daily.component.html',
   styleUrls: ['./daily.component.scss']
 })
-export class DailyComponent implements OnInit {
+export class DailyComponent implements OnInit, OnDestroy {
 
   forecast!: IForecast;
   location: string = 'auto:ip';
-  currentTime?: string;
   airQualityIndex?: string;
   airQualityText?: string;
-
+  subscriber?: Subscription;
   constructor(
     private sForecast: ForecastService,
-    private sHeader: HeaderService
+    private router: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
-    this.forecast = this.sHeader.forecast!;
-    this.airQualityIndex = (this.forecast.current.air_quality['us-epa-index']).toString();
-    const air: any = this.sForecast.airQualityCode();
-    this.airQualityText = air[this.airQualityIndex]
-    this.currentTime = this.getCurrentTime();
+    this.getForecast();
+    this.location = this.router.snapshot.params['city'];
   }
 
-  getCurrentTime() {
-    return new Date().getHours() + ':' + new Date().getMinutes();
+  getForecast() {
+    this.subscriber = this.sForecast.getCurrentLocationForecast(this.location)
+        .subscribe((res) => {
+          this.forecast = res;
+          this.getAirQuality(res);
+        });
+  }
+  
+  getAirQuality(forecast: IForecast) {
+    this.airQualityIndex = (forecast.current.air_quality['us-epa-index']).toString();
+    const air: any = this.sForecast.airQualityCode();
+    this.airQualityText = air[this.airQualityIndex];
+  }
+
+  ngOnDestroy() {
+    this.subscriber?.unsubscribe();
   }
 }
+
