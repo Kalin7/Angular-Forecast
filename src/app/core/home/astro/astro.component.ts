6faa4da8092ba75208025f39d8f5718c8352a4df
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-06-23 12:36:35 +0300

    uploaded components
    added home component and his children's components Astro and city

---
+++ src/app/core/home/astro/astro.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+import { IAstroInfo } from '../../interfaces';
+import { AstroService } from '../../service/astro.service';
+
+@Component({
+  selector: 'app-astro',
+  templateUrl: './astro.component.html',
+  styleUrls: ['./astro.component.scss']
+})
+export class AstroComponent implements OnInit {
+
+  astro?: IAstroInfo;
+  constructor(
+    library: FaIconLibrary,
+    private sAstro: AstroService,
+  ) {
+    library.addIcons(
+      faSun,
+      faMoon
+    )
+   }
+
+  ngOnInit(): void {
+    this.getAstro();
+  }
+
+  getAstro() {
+    this.sAstro.getAstrology().subscribe((res) => {
+      this.astro = res.astronomy;
+      console.log(this.astro);
+    })
+  }
+
+}
+function faSunrise(faSunrise: any) {
+  throw new Error('Function not implemented.');
+}
+
---
+++ src/app/core/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { IForecast } from '../interfaces';
+import { ForecastService } from '../service/forecast.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  forecast: IForecast[] = [];
+  cities: string[] = ['Sofia', 'Plovdiv', 'Varna'];
+  
+  constructor(
+    private sForecast: ForecastService
+  ) { }
+
+  ngOnInit(): void {
+    this.getForecasts();
+  }
+
+  getForecasts() {
+    this.cities.map((city) => {
+      this.sForecast.getMultipleCitiesForecast(city)
+          .subscribe((res) => {
+            this.forecast.push(res);
+      });
+    });
+  }
+
+}
