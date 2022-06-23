363fa4a907ae0b739aa65216982aab9d9f14f1b7
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-06-23 12:34:18 +0300

    updated component
    added location to the header service

--- src/app/core/header/search/search.component.ts
+++ src/app/core/header/search/search.component.ts
@@ -2,9 +2,8 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormGroup, FormControl} from '@angular/forms';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { IForecast, ILocation } from '../../interfaces';
-import { ForecastService } from '../../service/forecast.service';
 import { HeaderService } from '../../service/header.service';
+
 
 @Component({
   selector: 'app-search',
@@ -14,17 +13,13 @@ import { HeaderService } from '../../service/header.service';
 export class SearchComponent implements OnInit {
 
   @Output() searchValue = new EventEmitter();
-
-  forecast?: IForecast;
-  currentLocation?: ILocation;
-
+ 
   form = new FormGroup({
     location: new FormControl(null)
   });
   
   constructor(
     library: FaIconLibrary,
-    private sForecast: ForecastService,
     private sHeader: HeaderService
   ) { 
     library.addIcons(
@@ -38,6 +33,7 @@ export class SearchComponent implements OnInit {
   onSearch() {
     const loc = this.form.get('location')!.value;
     this.searchValue.emit(loc);
+    this.sHeader.getLocation(loc!);
   }
 
 }
