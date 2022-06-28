c5de3e519c84624abe89bce225afe469230f25b7
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-06-28 14:02:52 +0300

    Add files via upload

---
+++ src/app/core/home/sports-news/sports-news.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faClock, faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { Observable } from 'rxjs';
+import { SportService } from '../../service/sport.service';
+
+@Component({
+  selector: 'app-sports-news',
+  templateUrl: './sports-news.component.html',
+  styleUrls: ['./sports-news.component.scss']
+})
+export class SportsNewsComponent implements OnInit{
+
+  football$: Observable<any>
+  countryName!: string;
+  isVisibleDetails?: boolean = false;
+ 
+  constructor(
+    library: FaIconLibrary,
+    private sSport: SportService,
+    private router: Router,
+    ) {
+      library.addIcons(
+        faGlobe,
+        faClock
+      )
+      this.football$ = this.sSport.getSports();
+     }
+
+  ngOnInit(): void {
+  }
+
+  show() {
+    this.isVisibleDetails = true
+  }
+  hide() {
+    this.isVisibleDetails = false
+    this.router.navigate(['/forecast/home']);
+  }
+}
