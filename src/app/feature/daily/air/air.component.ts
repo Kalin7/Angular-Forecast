44ca3a7f11f38e1be5b70b2763b7aafc928beab0
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-06-29 17:38:41 +0300

    added component air

---
+++ src/app/feature/daily/air/air.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IAirQuality } from 'src/app/core/interfaces';
+import { ForecastService } from 'src/app/core/service/forecast.service';
+
+@Component({
+  selector: 'app-air',
+  templateUrl: './air.component.html',
+  styleUrls: ['./air.component.scss']
+})
+export class AirComponent implements OnInit {
+
+  @Input() air!: IAirQuality
+  condition?: any;
+
+  constructor(
+    private sAir: ForecastService
+  ) { }
+
+  ngOnInit(): void {
+    this.condition = this.sAir.airQualityCode(this.air['us-epa-index']);
+  }
+
+  
+}
