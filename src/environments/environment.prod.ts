4a455eefba349213d189775eb5aaeeee8e8872d4
Kalin7 <kalin.kirov7@gmail.com>
2022-07-05 09:47:27 +0300

    build created

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  baseUrl: " https://api.weatherapi.com/v1",
+  mapUrl: "https://www.google.com/maps/embed/v1/place?key=AIzaSyCZpXmJiorqoVXvXyuUaH9XdeOqQuIc2YA&q=",
+  secretKey: "7ae18fe12717423387d53415220606",
+  
+};
